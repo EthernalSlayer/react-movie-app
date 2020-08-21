0f0ace01d13fbc85cf3cf8ce5ebb6d5ad1e394a2
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-08-21 12:00:01 +0200

    add form and input

--- src/FormMovie.js
+++ src/FormMovie.js
@@ -11,8 +11,53 @@ class FormMovie extends React.Component {
     };
   }
 
+  handleChange = (e) => {
+    console.log(e);
+  };
+
   render() {
-    return <div className="FormMovie">Movie</div>;
+    return (
+      <div className="FormMovie">
+        <h1>Saisie d'un film</h1>
+
+        <form>
+          <fieldset>
+            <legend>Informations</legend>
+            <div className="form-data">
+              <label htmlFor="title">Titre</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                onChange={this.handleChange}
+                value={this.state.title}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="url">Poster</label>
+              <input
+                type="text"
+                id="url"
+                name="url"
+                onChange={this.handleChange}
+                value={this.state.url}
+              />
+            </div>
+
+            <div className="form-data">
+              <label htmlFor="comment">Commentaires</label>
+              <textarea
+                id="comment"
+                name="comment"
+                onChange={this.handleChange}
+                value={this.state.comment}
+              />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+    );
   }
 }
 
